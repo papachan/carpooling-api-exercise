fcc3ae01c51647495731302e194c88d5f61d41cd
Dan Loaiza <papachan@gmail.com>
2019-06-20 17:22:21 -0500

    better tests parsing response data with cheshire json

--- test/mobytronics/server_test.clj
+++ test/mobytronics/server_test.clj
@@ -2,16 +2,19 @@
   (:require [clojure.test :refer :all]
             [ring.mock.request :refer [request json-body]]
             [mobytronics.router :refer [app]]
-            [muuntaja.core :as m]))
-
+            [cheshire.core :as json]))
 
 (deftest server-api-test
-  (testing "GET /passengers request"
-    (let [response (-> (request :get "/passengers") app)]
-      (is (= (:status response) 200)
-          (= (:headers response) "{Content-Type application/json; charset=utf-8}"))))
+  (testing "GET /passengers endpoint"
+    (let [response (-> (request :get "/passengers") app)
+          json-response (json/parse-string (slurp (:body response)) true)]
+      (is (= (:status response) 200))
+      (is (= (:headers response) {"Content-Type" "application/json; charset=utf-8"}))
+      (is (some? (:response json-response)))))
 
-  (testing "GET /drivers request"
-    (let [response (-> (request :get "/drivers") app)]
-      (is (= (:status response) 200)
-          (= (:headers response) "{Content-Type application/json; charset=utf-8}")))))
+  (testing "GET /drivers endpoint"
+    (let [response (-> (request :get "/drivers") app)
+          json-response (json/parse-string (slurp (:body response)) true)]
+      (is (= (:status response) 200))
+      (is (= (:headers response) {"Content-Type" "application/json; charset=utf-8"}))
+      (is (some? (:response json-response))))))
